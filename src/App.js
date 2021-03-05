3d45a62c6dfb93f18d6b470992740b6546afaa7e
Arielle Loring <arieloring@gmail.com>
2021-03-05 09:57:27 -0700

    fucked up, single page ayooooooo and now half the shit works

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import './App.css';
-import Words from './components/Words/Words';
-import Chords from './components/Chords/Chords';
-import Instrument from './components/Instrument/Instrument';
+import chordData from './chordData';
 
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          Generate a Song
-        </h1>
-        <Instrument/>
-        <Chords />
-        <Words />
-        <button>
-          Click Here
-        </button>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      instrument: '',
+      chords: chordData[Math.floor(Math.random() * (chordData.length - 1))],
+      words: []};
+  }
+
+  selectInstrument() {
+    if (this.state.instrument === ''){
+      const instruments = ['guitar', 'ukulele'];
+      let selectNum = Math.floor(Math.random() * 2);
+      let selectedInstrument = instruments[selectNum];
+      this.setState({
+        instrument: selectedInstrument
+      });
+    }
+
+  }
+
+  render() {
+    this.selectInstrument()
+    return (
+      <div>
+        <h1>Generate a song!</h1>
+        <h2>{this.state.instrument}</h2>
+        <h2>{this.state.chords}</h2>
+
+        <button>Click Here</button>
+      </div>
+    );
+  }
 }
+
+
 
 export default App;
---
+++ src/chordData.js
@@ -0,0 +1,11 @@
+const chordData = [
+['C,G,Amin,F'],
+['Amin, F, C, G'],
+['C, F, G, C'],
+['C, F, G, C'],
+['C, Amin, F, G'],
+['C, F, C, G'],
+['C, Emin, F, G']
+];
+
+export default chordData;
